9490f6d1d56d3aed759fa791de765e4a0065d050
Luca-Rubino <lucasrubino92@gmail.com>
2024-08-20 18:18:33 +0200

    Insert authentication sistem

---
+++ resources/js/alert_confirm.js
@@ -0,0 +1,10 @@
+document.getElementById('creation_form').addEventListener('submit', function( event ){
+
+    event.preventDefault();
+
+
+    if(window.confirm('Sei sicuro di voler procedere?') === true){
+        this.submit();
+    }
+})
+
---
+++ resources/js/delete_confirm.js
@@ -0,0 +1,13 @@
+const deleteForms = document.querySelectorAll('form.delete_form');
+
+deleteForms.forEach((deleteFormElement) =>{
+    deleteFormElement.addEventListener('submit', function( event ){
+        event.preventDefault();
+        const name = this.getAttribute('data_project_nome');
+
+        if(window.confirm(`Sei sicuro di voler cancellare ${name}?` ) === true){
+            this.submit();
+        }
+    });
+
+})
